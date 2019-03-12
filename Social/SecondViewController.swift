166faf94b9901c41ab14e7dbc920d16f50155e1f
ahmed abokhalil <aabokhalil@gmail.com>
2019-03-12 09:27:34 +0200

    Initial Commit

---
+++ Social/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Social
+//
+//  Created by ahmed abokhalil on 7/5/1440 AH.
+//  Copyright © 1440 AH ahmed abokhalil. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
